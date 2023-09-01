26d2c82a9e85f7dc9fab28c4197390b0b8eec065
goku-raj <anshuraj1479@gmail.com>
2023-09-01 16:24:07 +0530

    main carousel

---
+++ src/scenes/home/MainCarousel.jsx
@@ -0,0 +1,121 @@
+import React, { useEffect, useState } from 'react';
+import { Box, Typography, IconButton, useMediaQuery } from "@mui/material";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import NavigateBeforeIcon from "@mui/icons-material/NavigateBefore";
+import NavigateNextIcon from "@mui/icons-material/NavigateNext";
+import { shades } from "../../theme";
+
+// List of image file paths
+const imagePaths = [
+    "../../assets/speaker.jpeg",
+    "../../assets/earbuds.jpeg",
+    "../../assets/smartwatch.jpeg",
+    "../../assets/speaker1.jpg",
+    "../../assets/headphone.jpeg",
+];
+
+// Function to import all images
+async function importAllImages() {
+  const images = {};
+
+  for (const path of imagePaths) {
+    const module = await import(path /* @vite-ignore */);
+    images[path] = module.default;
+  }
+
+  return images;
+}
+
+const MainCarousel = () => {
+  const [heroTextureImports, setHeroTextureImports] = useState({});
+  const isNonMobile = useMediaQuery("(min-width:600px)");
+
+  useEffect(() => {
+    async function fetchImages() {
+      const images = await importAllImages();
+      setHeroTextureImports(images);
+    }
+
+    fetchImages();
+  }, []);
+
+  return (
+    <Carousel
+      infiniteLoop={true}
+      showThumbs={false}
+      showIndicators={false}
+      showStatus={false}
+      renderArrowPrev={(onClickHandler, hasPrev, label) => (
+        <IconButton
+          onClick={onClickHandler}
+          sx={{
+            position: "absolute",
+            top: "50%",
+            left: "0",
+            color: "white",
+            padding: "5px",
+            zIndex: "10",
+          }}
+        >
+          <NavigateBeforeIcon sx={{ fontSize: 40 }} />
+        </IconButton>
+      )}
+      renderArrowNext={(onClickHandler, hasNext, label) => (
+        <IconButton
+          onClick={onClickHandler}
+          sx={{
+            position: "absolute",
+            top: "50%",
+            right: "0",
+            color: "white",
+            padding: "5px",
+            zIndex: "10",
+          }}
+        >
+          <NavigateNextIcon sx={{ fontSize: 40 }} />
+        </IconButton>
+      )}
+    >
+      {Object.values(heroTextureImports).map((texture, index) => (
+        <Box key={`carousel-image-${index}`}>
+          <img
+            src={texture}
+            alt={`carousel-${index}`}
+            style={{
+              width: "100%",
+              height: "700px",
+              objectFit: "cover",
+              backgroundAttachment: "fixed",
+            }}
+          />
+          <Box
+            color="white"
+            padding="20px"
+            borderRadius="1px"
+            textAlign="left"
+            backgroundColor="rgb(0, 0, 0, 0.4)"
+            position="absolute"
+            top="46%"
+            left={isNonMobile ? "10%" : "0"}
+            right={isNonMobile ? undefined : "0"}
+            margin={isNonMobile ? undefined : "0 auto"}
+            maxWidth={isNonMobile ? undefined : "240px"}
+          >
+            <Typography color={shades.secondary[200]}>-- NEW ITEMS</Typography>
+            <Typography variant="h1">Summer Sale</Typography>
+            <Typography
+              fontWeight="bold"
+              color={shades.secondary[300]}
+              sx={{ textDecoration: "underline" }}
+            >
+              Discover More
+            </Typography>
+          </Box>
+        </Box>
+      ))}
+    </Carousel>
+  );
+};
+
+export default MainCarousel;
